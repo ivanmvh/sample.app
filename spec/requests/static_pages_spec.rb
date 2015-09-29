a3022ed7b61d5d94ae8ff3486feb6030c166bb98
ivanmvh <ivanmvh@gmail.com>
2015-09-29 08:38:31 -0430

    Cap 5.6 - Ejercicios - Mejoras y simplificacion de tests
    - shared_examples
    - let (:variable) {valor}
    - page.should
    - click_link etiquetaLink
    - uso include en spec\support\utilities.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,64 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   subject {page}
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
 
   describe "Home page" do
     
     before {visit root_path}
+    let (:heading) {'Sample App'}
+    let (:page_title) {''}
 
-    it { should have_selector('h1', text: 'Sample App')}
-
-    it {should have_selector('title', text: full_title('') )}
-    
+    it_should_behave_like 'all static pages'    
     it {should_not have_selector('title', text: full_title('Home'))}
   end
 
   describe "Help page" do
     before {visit help_path} 
-    it {should have_selector('h1', text: 'Help')}
-    
-    it {should have_selector('title',text: "#{base_title} | Help")}
-  end
+
+    let (:heading) {'Help'}
+    let (:page_title) {'Help'}
+
+    it_should_behave_like 'all static pages'
+  end   
 
   describe "pagina 'Acerca de'" do
     before {visit about_path} 
 
-    it {should have_selector('h1', text: 'Acerca de')}
-  
-    it {should have_selector('title',text: full_title('Acerca de') )}
+    let (:heading) {'Acerca de'}
+    let (:page_title) {'Acerca de'}
+
+    it_should_behave_like 'all static pages'
   end
 
   describe "pagina 'Contacto'" do
     before {visit contacto_path}
 
-    it {should have_selector('h1', text: 'Contacto')}
-  
-    it {should have_selector('title',text: full_title('Contacto')) }
+    let (:heading) {'Contacto'}
+    let (:page_title) {'Contacto'}
+
+    it_should_behave_like 'all static pages'
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link 'About'
+    page.should have_selector 'title', text: full_title('Acerca de')
+    click_link 'Help'
+    page.should have_selector 'title', text: full_title('Help')
+    click_link 'Contacto'
+    page.should have_selector 'title', text: full_title('Contacto')
+    click_link 'Home'
+    page.should have_selector 'title', text: full_title('')
+    click_link 'Sign up now!'
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link 'sample app'
+    page.should have_selector 'title', text: full_title('')
   end
 end
